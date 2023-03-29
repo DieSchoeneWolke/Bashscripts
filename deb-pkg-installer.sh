9dab52e1dd07f5ede6f3a738a8a977c66bce9b70
Jan Schönfeld <117728997+DieSchoeneWolke@users.noreply.github.com>
2023-03-30 00:23:05 +0200

    root check added
    Added a check to ensure that the script is run as the root user.

--- deb-pkg-installer.sh
+++ deb-pkg-installer.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+# Check if the user is root
+if [ ! "$(whoami)" = root ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 # Update and upgrade packages
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
+
 # Ask for package name
 echo "Enter a package name to install:"
 read package
+
 # Install package
-sudo apt install $package -y
+apt install $package -y
